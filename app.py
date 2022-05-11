899c98af193d54054cf64e69077f9571b1d5c052
jeonghyeonuk <junghunwook456@naver.com>
2022-05-11 11:15:31 +0900

    app.py 메인페이지 연결 추가

--- app.py
+++ app.py
@@ -4,8 +4,6 @@
 # def home():
 #     return render_template('index.html')
 
-
-
 # package import
 from flask import Flask, render_template, request, jsonify, redirect, url_for
 import requests
@@ -22,15 +20,44 @@ from pymongo import MongoClient
 client = MongoClient('mongodb+srv://test:test@cluster0.mai7p.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
 
+import certifi
+
+ca = certifi.where()
 # 크롤링
 import requests
 from bs4 import BeautifulSoup
 
 @app.route('/')
-def main():
-    # DB에서 저장된 단어 찾아서 HTML에 나타내기
-    words = list(db.words.find({}, {"_id": False}))
-    return render_template("index.html", words=words)
+def home():
+    return render_template('index.html')
+
+url = 'https://www.skyscanner.co.kr/news/inspiration/top10-europe-destinations-where-koreans-love-to-go'
+
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get(url,headers=headers)
+
+soup = BeautifulSoup(data.text, 'html.parser')
+
+card_image = soup.select('#post-321 > div > div > div.entry-content > figure')
+card_title = soup.select('#post-321 > div > div > div.entry-content > h2')
+count = 0
+
+for image,title in zip(card_image,card_title):
+    count = + 1
+    image = image.select_one('img')['src']
+    title = title.select_one('a').text
+    print(image, title)
+    doc = {
+        'post_num': count,
+        'title': title,
+        'image_link': image
+        }
+    db.countrys.insert_one(doc)
+
+@app.route("/countrys", methods=["GET"])
+def countrys_get():
+    countrys = list(db.countrys.find({}, {'_id': False}))
+    return jsonify({'countrys':countrys})
 
 @app.route('/login')
 def login():
@@ -63,4 +90,4 @@ def save_comment():
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5500, debug=True)
